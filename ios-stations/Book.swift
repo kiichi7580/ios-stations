4fadf7ae2be055381fe001af1cc08cfb95e3e150
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-03-14 16:52:37 +0900

    Book構造体に本の情報を保持できるようにプロパティを追加

--- ios-stations/Book.swift
+++ ios-stations/Book.swift
@@ -4,4 +4,13 @@
 //
 
 struct Book: Decodable {
+    let id: String
+    let title: String
+    let url: String
+    let detail: String
+    let review: String
+    let reviewer: String
+    let isMine: Bool
 }
+
+
