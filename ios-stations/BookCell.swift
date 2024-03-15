60a1c459dde035c58fc97085b9a2094ae509b811
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-03-15 21:13:46 +0900

    UITableViewの各行で、Bookインスタンスを適用したBookCellを返すようにする

--- ios-stations/BookCell.swift
+++ ios-stations/BookCell.swift
@@ -8,4 +8,8 @@ import UIKit
 class BookCell: UITableViewCell {
     
     var element: Book!
+    
+    @IBOutlet weak var leadingImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
 }
--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -13,27 +13,54 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // fetchBooksメソッドを呼び出し、APIからデータを取得
-               bookAPIClient.fetchBooks(offset: 0) { [weak self] fetchedBooks in
-                   if let fetchedBooks = fetchedBooks {
-                       // データの取得が成功した場合、booksプロパティに保存
-                       self?.books = fetchedBooks
-                       // データを使ってテーブルビューを更新などの処理を行う
-                       // ここに処理を追加
-                   } else {
-                       // データの取得が失敗した場合の処理
-                       print("Failed to fetch books.")
-                   }
-               }
+            bookAPIClient.fetchBooks(offset: 10) { [weak self] fetchedBooks in
+                if let fetchedBooks = fetchedBooks {
+                    // データの取得が成功した場合、booksプロパティに保存
+                    self?.books = fetchedBooks
+                    // データを使ってテーブルビューを更新などの処理を行う
+                } else {
+                    // データの取得が失敗した場合の処理
+                    print("Failed to fetch books.")
+                }
+            }
     }
 }
 
 extension FirstViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        // BookCellを再利用可能な形で取得
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as? BookCell else {
+            return UITableViewCell() // キャストに失敗した場合はデフォルトのセルを返す
+        }
+        
+        
+        
+        // セルにデータを設定
+        if let book = books?[indexPath.row] {
+            guard let imageUrl = URL(string: book.url) else {
+                print("Invalid URL")
+                return UITableViewCell()// URLが無効な場合は空のセルが返ってくる
+            }
+            
+            if let imageData = try? Data(contentsOf: imageUrl) {
+                    // URLからデータを取得できた場合はUIImageを作成してUIImageViewに設定
+                    let image = UIImage(data: imageData)
+                        cell.leadingImage.image = image
+                } else {
+                    // URLからデータを取得できなかった場合はデフォルトの画像を設定するなどの処理を行う
+                    cell.leadingImage.image = UIImage(named: "Book")
+                }
+//            cell.leadingImage.image = UIImage(image: imageUrl)
+            cell.titleLabel.text = book.title
+            cell.detailLabel.text = book.detail
+            // 他のプロパティも設定可能
+        }
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return books?.count ?? 0
     }
 }
 
